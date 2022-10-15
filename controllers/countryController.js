150f8b4ac243f29172c15ffb638bfde8def03a86
EJ Milord <jmilord7@gmail.com>
2022-10-15 18:28:34 -0400

    moved fetching data from routes to controller

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -1,18 +1,77 @@
 const fetch = require('node-fetch')
 
 // Display All Countries
-const countriesView = (req,res) => {
-    res.render("index", {})
+const countriesView = async (req,res) => {
+    const api_url = 'https://restcountries.com/v3.1/all'
+    const fetch_response = await fetch(api_url)
+    const json = await fetch_response.json()
+    //console.log(json)
+    res.render('index.ejs',{info:json})
+}
+
+const regionView = async (req,res) => {
+    const region = req.query.region
+    const api_url = 'https://restcountries.com/v3.1/region/'+ region
+    const fetch_response = await fetch(api_url)
+    const json = await fetch_response.json()
+    res.render('index.ejs',{info:json})
+}
+
+const searchView = async(req,res) => {
+    const name = req.query.name
+    console.log(name)
+    const api_url = 'https://restcountries.com/v3.1/name/'+ name
+    const fetch_response = await fetch(api_url)
+    const json = await fetch_response.json()
+    res.render('index.ejs',{info:json})
 }
 
 // Display single page with country info
-const countryInfoView = (req,res) => {
-    res.render("country", {})
+const countryInfoView = async (req,res) => {
+    const country = req.query.country;
+    const api_url = 'https://restcountries.com/v3.1/name/'+ country + '?fullText=true'
+    const fetch_response = await fetch(api_url)
+    const countryData = await fetch_response.json()
+   
+    const nativeName = Object.values(countryData[0].name.nativeName)
+    const languages = Object.values(countryData[0].languages)
+    const currency = Object.values(countryData[0].currencies)
+    let borderData = null
+  
+    if(countryData[0].borders != undefined)
+    {
+        borderData = await getBorderCountries(countryData[0].borders)
+    }
+    
+    res.render('country.ejs', {
+        data: countryData,
+        nativeName:nativeName,
+        languages: languages,
+        currency: currency,
+        borderData: borderData
+    })
 }
 
+async function getBorderCountries(borderCountries)
+{
+    let borders = []
+    for(let i = 0; i < borderCountries.length; i++)
+    {
+        const api_url = 'https://restcountries.com/v3.1/alpha?codes='+ borderCountries[i]
+        const fetch_response = await fetch(api_url)
+        const country = await fetch_response.json()
+        //console.log(country[0].name.common)
+        borders.push(country[0].name.common)
+    }
+
+    // console.log(borders)
+    return borders
+}
 
 
 module.exports = {
     countriesView,
     countryInfoView,
+    searchView,
+    regionView
 }
--- routes/country.js
+++ routes/country.js
@@ -3,87 +3,20 @@ const express = require('express')
 // const {countriesView, countryInfoView} = require('../controllers/countryController');
 const router = express.Router()
 const fetch = require('node-fetch')
-const { africaView } = require('../controllers/countryController')
-const bodyParser = require('body-parser')
+const { countriesView, countryInfoView, searchView, regionView} = require('../controllers/countryController')
 
 router.use(express.urlencoded({extended: true}))
 router.use(express.json())
 
+router.get('/', countriesView)
 
-// router.get('/', countriesView)
+router.get('/filter', regionView)
 
-router.get('/', async(req, res) => {
-    const api_url = 'https://restcountries.com/v3.1/all'
-    const fetch_response = await fetch(api_url)
-    const json = await fetch_response.json()
-    //console.log(json)
-    res.render('index.ejs',{info:json})
-})
+router.get('/search', searchView)
+
+router.get('/countryData', countryInfoView)
 
 
-router.get('/filter', async(req,res) => {
-    const region = req.query.region
-    console.log(region)
-    const api_url = 'https://restcountries.com/v3.1/region/'+ region
-    const fetch_response = await fetch(api_url)
-    const json = await fetch_response.json()
-    res.render('index.ejs',{info:json})
-})
-
-router.get('/search', async(req,res) => {
-    const name = req.query.name
-    console.log(name)
-    const api_url = 'https://restcountries.com/v3.1/name/'+ name
-    const fetch_response = await fetch(api_url)
-    const json = await fetch_response.json()
-    res.render('index.ejs',{info:json})
-})
-
-router.get('/countryData', async(req, res) => {
-    const country = req.query.country;
-    const api_url = 'https://restcountries.com/v3.1/name/'+ country + '?fullText=true'
-    const fetch_response = await fetch(api_url)
-    const countryData = await fetch_response.json()
-   
-    const nativeName = Object.values(countryData[0].name.nativeName)
-    const languages = Object.values(countryData[0].languages)
-    const currency = Object.values(countryData[0].currencies)
-    //const borders = Object.values(countryData[0].borders)
-    let borderData = null
-    //console.log(countryData[0].borders) 
-    if(countryData[0].borders != undefined)
-    {
-        borderData = await getBorderCountries(countryData[0].borders)
-    }
-    
-    // console.log(countryData[0].borders)     
-    //console.log(borderData)
-    // console.log(countryData[0].name.nativeName.language.official)
-    // const nativeName = JSON.parse(countryData[0].name.nativeName)
-    res.render('country.ejs', {
-        data: countryData,
-        nativeName:nativeName,
-        languages: languages,
-        currency: currency,
-        borderData: borderData
-    })
-})
-
-async function getBorderCountries(borderCountries)
-{
-    let borders = []
-    for(let i = 0; i < borderCountries.length; i++)
-    {
-        const api_url = 'https://restcountries.com/v3.1/alpha?codes='+ borderCountries[i]
-        const fetch_response = await fetch(api_url)
-        const country = await fetch_response.json()
-        //console.log(country[0].name.common)
-        borders.push(country[0].name.common)
-    }
-
-    // console.log(borders)
-    return borders
-}
 
 
 
